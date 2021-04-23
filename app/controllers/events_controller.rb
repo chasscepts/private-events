6b1c2999b15792745211666e645e4b5f12c73d85
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-23 17:56:33 +0300

    Add display of creator for show event page, add event index page to dispaly all events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,11 +13,20 @@ class EventsController < ApplicationController
 
     def create
         user = User.find(session[:current_user_id])
-        @event = user.created_events.build(post_params)
+        @event = user.created_events.build(event_params)
         if @event.save
           redirect_to root_path
         else
           render :new
         end
+    end
+
+    private
+
+    def event_params
+
+        params.require(:event).permit([:description])
+
+
     end
 end
